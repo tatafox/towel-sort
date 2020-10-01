28a24568e61206927fe1188a65665c1362d8a90e
Ваше имя@ <you@example.com>
2020-10-01 17:01:15 +0300

    fix: error result

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-    if (matrix == undefined) {
+    if (!matrix) {
         return [];
     }
 
@@ -10,12 +8,14 @@ module.exports = function towelSort (matrix) {
 
     for(let i = 0; i < matrix.length; i++){
         if(i % 2 === 1){
-            result.concat(matrix[i].reverse());
+            result = result.concat(matrix[i].reverse());
         } else {
-            result.concat(matrix[i]);  
+            result = result.concat(matrix[i]);  
         }
       }
-      
+
       return result;
 
 }
+
+
